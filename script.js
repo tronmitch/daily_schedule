caefb997411b46ec5445a39a328b22dbd0b42b46
tronmitch <theron.mitchell@gmail.com>
2023-12-06 23:45:44 -0700

    added functionality to populate schedule from local storage

--- script.js
+++ script.js
@@ -5,23 +5,31 @@ $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
+  // function? How can DOM traversal be used to get the 'hour-x' id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+
   var saveButtonEl = $('.saveBtn')
-  var descriptionEl = $('.description')
+
   var toDoList = []
-  saveButtonEl.on('click',function(){
+  if (window.localStorage.getItem('scheduleKey')) {
+    toDoList = JSON.parse(window.localStorage.getItem('scheduleKey').valueOf())
+  }
+  var x
+  saveButtonEl.on('click', function () {
     var target = $(this).parent().attr('id')
+    var description = $("#" + target).children(".description").val()
     var toDo = {
       hour: target,
-      toDoDescription: descriptionEl.val()
+      toDoDescription: description
     }
-    
-    toDoList = toDoList.map(function(toDo){
-      toDoList.push(toDo)
-    })
-    window.localStorage.setItem('key', JSON.stringify(toDoList));
+    if (toDoList.some(e => e.hour == target)) {
+      toDoList = toDoList.filter(e => e.hour !== target)
+      // toDoList.find(e => e.)
+    }
+    toDoList.push(toDo)
+
+    window.localStorage.setItem('scheduleKey', JSON.stringify(toDoList));
     console.log(toDoList)
   })
 
@@ -32,27 +40,56 @@ $(function () {
   // current hour in 24-hour time?
 
 
-var today = dayjs();
-var currentHour = today.$H;
-var x = $('#hour-13');
-x.attr('class','row time-block past')
+  var today = dayjs();
+  var currentHour = today.$H;
 
-for (let i = 9; i <15; i++){
+  //Cycle through hours 9-17(ie 15 = 5pm)
+  for (let i = 9; i < 17; i++) {
 
-  var hourBlock = $('#hour-'+i)
-  if (currentHour == i){
-    hourBlock.attr('class','row time-block present')
-  }else if(currentHour < i){
-    hourBlock.attr('class','row time-block past')
-  }else{
-    hourBlock.attr('class','row time-block future')
+    var hourBlock = $('#hour-' + i)
+    if (currentHour == i) {
+      hourBlock.attr('class', 'row time-block present')
+    } else if (currentHour < i) {
+      hourBlock.attr('class', 'row time-block past')
+    } else {
+      hourBlock.attr('class', 'row time-block future')
+    }
+
   }
+  {
 
 }
-  //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+  for (let i=0; i < toDoList.length; i++) {
+    var previousTodo = toDoList[i].toDoDescription
+    $("#"+toDoList[i].hour).children('.description').text(toDoList[i].toDoDescription)
+  }
+
+
+
+
   // TODO: Add code to display the current date in the header of the page.
+  days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+  months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  var ordinal = 'th'
+  lastDigitOfDay = today.format('DD').slice(-1)
+  if (lastDigitOfDay == 1) {
+    ordinal = 'st'
+  }
+  if (lastDigitOfDay == 2) {
+    ordinal = 'nd'
+  }
+  if (lastDigitOfDay == 3) {
+    ordinal = 'rd'
+  }
+  //Format the date by using the position in arrays of the Days and Months
+  var dateString = days[today.$W] + ", " + months[today.$M] + " " + today.$D + ordinal
+
+  //Alternative use dayjs built in formating
+  dateString = today.format('dddd') + ", " + today.format('MMMM') + " " + today.$D + ordinal
+
+  //Append the text to the header element
+  $('#currentDay').text(dateString)
 });
